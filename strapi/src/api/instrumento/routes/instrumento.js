519d64b6d97484a9c095f1f3868637d23f711068
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-04-27 12:58:51 -0300

    feat: first commit

---
+++ strapi/src/api/instrumento/routes/instrumento.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * instrumento router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::instrumento.instrumento');
---
+++ strapi/src/api/instrumento/services/instrumento.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * instrumento service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::instrumento.instrumento');
