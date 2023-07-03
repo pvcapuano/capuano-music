0466905db97d11259a8c50b9bd55e178097b15af
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-07-03 19:31:23 -0300

    refator: ajusting stripe config

--- front/src/pages/api/stripe.ts
+++ front/src/pages/api/stripe.ts
@@ -4,10 +4,21 @@ const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY || "", {
   apiVersion: "2020-08-27",
 });
 
+interface Item {
+  name: string;
+  price: number;
+  quantity: number;
+  image: {
+    asset: {
+      _ref: string;
+    };
+  }[];
+}
+
 export default async function handler(req: any, res: any) {
   if (req.method === "POST") {
     try {
-      const params = {
+      const params: Stripe.Checkout.SessionCreateParams = {
         submit_type: "pay",
         mode: "payment",
         payment_method_types: ["card"],
@@ -16,7 +27,7 @@ export default async function handler(req: any, res: any) {
           { shipping_rate: "shr_1NJkZiDRubp9Ec6zfDkeM3o2" },
           { shipping_rate: "shr_1NJkaDDRubp9Ec6zfYtek40e" },
         ],
-        line_items: req.body.map((item: any) => {
+        line_items: req.body.map((item: Item) => {
           const img = item.image[0].asset._ref;
           const newImage = img
             .replace(
@@ -51,7 +62,7 @@ export default async function handler(req: any, res: any) {
       console.log(session); // Verifique a sessão criada
 
       res.status(200).json(session);
-    } catch (err) {
+    } catch (err: any) {
       console.error(err);
       res
         .status(err.statusCode || 500)
